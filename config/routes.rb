4bff702a55f4d01ade6f77ddfeacf4885ef88a40
DavidLejeune <Lejeune.David@outlook.com>
2019-10-29 23:28:08 +0100

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :installations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "installations#index"
+  #root to: "installations#index"
+  root to: "pages#home"
   get '/about', to: 'pages#about'
+  get '/home', to: 'pages#home'
 end
