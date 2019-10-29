e39ec677f8275e1e331a51006790e7859546c747
DavidLejeune <Lejeune.David@outlook.com>
2019-10-29 01:37:55 +0100

    configure installations controller and application controler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+# Prevent CSRF attaks by raising an exception
+# for API , you lay want to use :null_session instead
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_ip, keys: [:username])
+end
+
 end
--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -1,5 +1,6 @@
 class InstallationsController < ApplicationController
   before_action :set_installation, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:show, index]
 
   # GET /installations
   # GET /installations.json
