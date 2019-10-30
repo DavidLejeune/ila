f2877096b218d73a74c1a5e93c2d14e7ccc0eccb
DavidLejeune <Lejeune.David@outlook.com>
2019-10-30 14:20:02 +0100

    comments model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :installations
+  resources :installations do
+    resources :comments
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root to: "installations#index"
+
+
   root to: "pages#home"
   get '/about', to: 'pages#about'
   get '/home', to: 'pages#home'
