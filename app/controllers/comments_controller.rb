e59da4df26900213f6c04b211a13f1ad5a98bbb5
DavidLejeune <Lejeune.David@outlook.com>
2019-10-31 15:59:44 +0100

    only show comments when available + linked comment to firendly id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
     def create
-		@installation = Installation.find(params[:installation_id])
+		@installation = Installation.friendly.find(params[:installation_id])
 		@comment = @installation.comments.create(params[:comment].permit(:name, :body))
 
 		redirect_to installation_path(@installation)
 	end
 
 	def destroy
-		@installation = Installation.find(params[:installation_id])
+		@installation = Installation.friendly.find(params[:installation_id])
 		@comment = @installation.comments.find(params[:id])
 		@comment.destroy
 
