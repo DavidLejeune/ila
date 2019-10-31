175b6ec78184dd3419979f69908980e2fc13e350
DavidLejeune <Lejeune.David@outlook.com>
2019-10-31 03:04:34 +0100

    working friendly id

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -1,5 +1,16 @@
 class Installation  < ActiveRecord::Base
+
+	extend FriendlyId
+	friendly_id :title, use: :slugged
+
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: {minimum: 5}
 	validates :description,  presence: true
+
+	def should_generate_new_friendly_id?
+		title_changed?
+	end
+	
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_142956) do
+ActiveRecord::Schema.define(version: 2019_10_31_015755) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 2019_10_30_142956) do
     t.index ["installation_id"], name: "index_comments_on_installation_id"
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
   create_table "installations", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -28,6 +39,8 @@ ActiveRecord::Schema.define(version: 2019_10_30_142956) do
     t.string "copyright"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_installations_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,8 +51,10 @@ ActiveRecord::Schema.define(version: 2019_10_30_142956) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   add_foreign_key "comments", "installations"
