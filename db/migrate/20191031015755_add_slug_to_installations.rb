576f0d0258193bb90fde99cc9e64608f435233a5
DavidLejeune <Lejeune.David@outlook.com>
2019-11-01 15:36:54 +0100

    cleaned

--- db/migrate/20191031015755_add_slug_to_installations.rb
+++
@@ -1,6 +0,0 @@
-class AddSlugToInstallations < ActiveRecord::Migration[6.0]
-  def change
-    add_column :installations, :slug, :string
-    add_index :installations, :slug, unique: true
-  end
-end
