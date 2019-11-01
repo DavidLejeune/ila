2161236478443b37e0ce539511ddf62f2bba67bf
DavidLejeune <Lejeune.David@outlook.com>
2019-11-01 15:38:29 +0100

    removed the cap fiasco

---
+++ db/migrate/20191029200013_create_installations.rb
@@ -0,0 +1,13 @@
+class CreateInstallations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :installations do |t|
+      t.string :title
+      t.text :description
+      t.string :credit
+      t.string :copyright
+      t.text :comment
+
+      t.timestamps
+    end
+  end
+end
