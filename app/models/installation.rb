ffb8a1d398f58da943e921dc1192d13d948f0557
DavidLejeune <Lejeune.David@outlook.com>
2019-11-03 23:29:19 +0100

    active storage works

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -1,2 +1,9 @@
 class Installation < ApplicationRecord
+
+    has_one_attached :image
+	has_many :comments, dependent: :destroy
+	validates :title, presence: true, length: {minimum: 5}
+	validates :description,  presence: true
+
+
 end
