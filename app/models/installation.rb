2b78e058ea8316843a4de66ce7cce0e89c123267
DavidLejeune <Lejeune.David@outlook.com>
2019-11-13 17:04:44 +0100

    working friendly id

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -1,9 +1,15 @@
 class Installation < ApplicationRecord
 
+	extend FriendlyId
+	friendly_id :title, use: :slugged
+
     has_one_attached :image
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: {minimum: 5}
 	validates :description,  presence: true
 
 
+	def should_generate_new_friendly_id?
+		title_changed?
+	end
 end
