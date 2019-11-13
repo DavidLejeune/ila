b1f04b9f8158e13f421014a8ed67528bb5df55ad
DavidLejeune <Lejeune.David@outlook.com>
2019-11-13 16:44:17 +0100

    basic working with comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+    def create
+		@installation = Installation.friendly.find(params[:installation_id])
+		@comment = @installation.comments.create(params[:comment].permit(:name, :body))
+
+		redirect_to installation_path(@installation)
+	end
+
+	def destroy
+		@installation = Installation.friendly.find(params[:installation_id])
+		@comment = @installation.comments.find(params[:id])
+		@comment.destroy
+
+		redirect_to installation_path(@installation)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :installations
+  resources :users
+  #resources :installations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # devise_for :users
-  # resources :installations do
-  #   resources :comments
-  # end
+  resources :installations do
+    resources :comments
+  end
   
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # #root to: "installations#index"
